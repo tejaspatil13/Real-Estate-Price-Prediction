0581b40d912492ce5adb00d6c9ef403799b76eb2
tejas patil <tejaspatil9284@gmail.com>
2025-07-13 13:19:44 +0530

    imp changes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,44 @@
+# from flask import Flask, request, jsonify
+# import util
+
+# app = Flask(__name__)
+
+# @app.route('/get_location_names', methods=['GET'])
+# def get_location_names():
+#     response = jsonify({
+#         'locations': util.get_location_names()
+#     })
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+
+#     return response
+
+# @app.route('/predict_home_price', methods=['GET', 'POST'])
+# def predict_home_price():
+#     total_sqft = float(request.form['total_sqft'])
+#     location = request.form['location']
+#     bhk = int(request.form['bhk'])
+#     bath = int(request.form['bath'])
+
+#     response = jsonify({
+#         'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+#     })
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+
+#     return response
+
+# if __name__ == "__main__":
+#     print("Starting Python Flask Server For Home Price Prediction...")
+#     util.load_saved_artifacts()
+#     app.run()
+
+
 from flask import Flask, request, jsonify
 import util
 
 app = Flask(__name__)
+
+# Load model/artifacts when the app starts
+util.load_saved_artifacts()
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -9,24 +46,23 @@ def get_location_names():
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
 
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+        estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
 
-    return response
+        response = jsonify({
+            'estimated_price': estimated_price
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    app.run()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
