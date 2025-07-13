1393e9fba305485c5e7a503c6df4812cd5485e15
tejas patil <tejaspatil9284@gmail.com>
2025-07-13 14:02:17 +0530

    added link for deploy

--- client/app.js
+++ client/app.js
@@ -26,8 +26,10 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
- var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
- // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+  var url = "https://real-estate-price-prediction-1-bbpa.onrender.com/predict_home_price";
+  
+   // Use this if  you are using nginx. i.e tutorial 8 and onwards
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -42,8 +44,8 @@ function onClickedEstimatePrice() {
 }
 function onPageLoad() {
   console.log( "document loaded" );
-   var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  //var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  //  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+   var url = "https://real-estate-price-prediction-1-bbpa.onrender.com/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
